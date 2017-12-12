4f7c97b0dd640485faf71b06967fbb68be16a028
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 09:58:22 +0100

    fini exercice8

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+ $first = 3 + 4;
+ $second = 5 * 20;
+ $third = 45 % 5;
+ echo 'Opération: '.$first.'</br> Multiplication: '.$second.'</br>Modulo: '.$third;
+ ?>
+ <!-- Créer 3 variables.
+Dans la première mettre le résultat de l'opération 3 + 4.
+Dans la deuxième mettre le résultat de l'opération 5 * 20.
+Dans la troisième mettre le résultat de l'opération 45 / 5.
+Afficher le contenu des variables. -->
